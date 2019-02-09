16fdbc10224af2130604710576038836df0a7c6a
Dana Kendybayeva <danka.kent@gmail.com>
2019-02-09 20:12:47 +0600

    1

---
+++ w2/w2_t1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_t1 
+{
+    class Program
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length <= 1)
+                return true;
+            else
+            {
+                if (s[0] != s[s.Length - 1])
+                    return false;
+                else
+                    return IsPalindrome(s.Substring(1, s.Length - 2));
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string s = File.ReadAllText(@"C:\Users\Swist\Desktop\c#\input1.txt");
+            bool pl = IsPalindrome(s);
+            if (pl == true) Console.WriteLine("Yes");
+            else Console.WriteLine("No");
+        }
+    }
+}
---
+++ w2/w2_t2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_t2
+{
+    class Program
+    {
+        static bool Prime(int a)
+        /*
+        this function checks is the number prime or not; 
+        true is prime and false is not prime
+        */
+        {
+            bool x = true;
+            if (a != 1)
+            {
+                for (int i = 2; i < a; ++i)
+                {
+                    if (a % i == 0)
+                    {
+                        x = false;
+                    }
+                }
+            }
+            else x = false;
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            string s = File.ReadAllText(@"C:\Users\Swist\Desktop\c#\input2.txt");
+            int n = Convert.ToInt16(s); // amount of numbers
+            int[] a = new int[n]; // creating an array of size n
+            string[] nums = Console.ReadLine().Split(new char[] { ' ' });
+            //spliting the given string of numbers into separate chars, so we can use them
+            int cnt = 0; //counter for prime numbers 
+            string answer = "";// string for output
+            for (int i = 0; i < n; ++i)
+            {
+                a[i] = int.Parse(nums[i]); // converting from string to int
+                if (Prime(a[i])) // checking if the number is prime or not
+                {
+                    cnt++; // if number is prime the counter increases for 1
+                    answer = answer + a[i].ToString() + " "; // getting ready the output
+                }
+            }
+            File.WriteAllText(@"C:\Users\Swist\Desktop\c#\output2.txt", answer);
+        }
+    }
+}
