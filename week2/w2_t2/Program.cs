5e84d3c52e3349daac6a1c95a6554063829b5d75
Dana Kendybayeva <danka.kent@gmail.com>
2019-02-11 17:20:20 +0600

    3

---
+++ week2/w2_t2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_t2
+{
+    class Program
+    {
+        static bool Prime(int a)
+        /*
+        this function checks is the number prime or not; 
+        true is prime and false is not prime
+        */
+        {
+            bool x = true;
+            if (a != 1)
+            {
+                for (int i = 2; i < a; ++i)
+                {
+                    if (a % i == 0)
+                    {
+                        x = false;
+                    }
+                }
+            }
+            else x = false;
+            return x;
+        }
+        static void Main(string[] args)
+        {
+            List<string> l = new List<string>();
+
+            FileStream fs = new FileStream(@"C:\Users\Swist\Desktop\c#\w2\input2.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+
+            string line = sr.ReadLine();
+            string[] nums = line.Split(' ');
+
+            foreach (var x in nums)
+            {
+                if (Prime(int.Parse(x)))
+                {
+                    l.Add(x);
+                }
+            }
+
+            sr.Close();
+            fs.Close();
+
+            FileStream fs2 = new FileStream(@"C:\Users\Swist\Desktop\c#\w2\output2.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs2);
+
+            foreach (var x in l)
+            {
+                sw.Write(x + " ");
+                Console.Write(x + " ");
+            }
+
+            sw.Close();
+            fs2.Close();
+        }
+    }
+}
---
+++ week2/w2_t4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace w2_t4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\test";
+            string path2 = @"C:\test2";
+            string fileName = "file.txt";
+            string destFile = System.IO.Path.Combine(path2, fileName);
+            DirectoryInfo di = new DirectoryInfo(path);
+            if(di.Exists)
+            {
+                di.Delete(true);
+            }
+            di.Create();
+
+            DirectoryInfo di2 = new DirectoryInfo(path2);
+            if (di2.Exists)
+            {
+                di2.Delete(true);
+            }
+            di2.Create();
+
+            StreamWriter sw = File.CreateText(@"C:\test\file.txt");
+            sw.Close();
+
+            if (Directory.Exists(path))
+            {
+                string[] files = Directory.GetFiles(path);
+
+                foreach (string s in files)
+                {
+                    fileName = Path.GetFileName(s);
+                    destFile = Path.Combine(path2, fileName);
+                    File.Copy(s, destFile, true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+
+            File.Move(destFile, @"C:\test2\file.txt");
+
+            if (Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.Delete(path, true);
+                }
+
+                catch (IOException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
