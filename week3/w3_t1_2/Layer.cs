d9df7b6313a083f18feaf7e8b4d3eefc1eeb4254
Dana Kendybayeva <danka.kent@gmail.com>
2019-02-12 20:54:52 +0600

    4

---
+++ week3/w3_t1_2/Layer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarManager3
+{
+    class Layer
+    {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+
+        private int selectedItem;
+        public int SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                if (value >= Content.Length)
+                {
+                    selectedItem = 0;
+                }
+                else if (value < 0)
+                {
+                    selectedItem = Content.Length - 1;
+                }
+                else
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+
+        
+
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                if (Content[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkCyan;
+                }
+                Console.WriteLine(i + 1 + ". " + Content[i].Name);
+            }
+        }
+    }
+}
+
